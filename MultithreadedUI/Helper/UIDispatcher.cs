e4e09c4fe599eb8301b95c41c6d16e5a6369f137
fanbanban <18261446150@163.com>
2018-12-12 11:44:07 +0800

    提交忽视代码

---
+++ MultithreadedUI/Helper/UIDispatcher.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace MultithreadedUI
+{
+    public static class UIDispatcher
+    {
+        public static HwndSource parentHwnd;
+        public static async Task<T> CreateElementAsync<T>(HwndSource hwndSource)
+            where T : Window, new()
+        {
+            parentHwnd = hwndSource;
+            return await CreateElementAsync<T>(() => new T());
+        }
+
+        private static async Task<T> CreateElementAsync<T>(Func<T> func)
+            where T : Window
+        {
+            if (func == null)
+                throw new ArgumentException(nameof(func));
+
+            var element = default(T);
+            Exception exception = null;
+            var are = new AutoResetEvent(false);
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    SynchronizationContext.SetSynchronizationContext(new DispatcherSynchronizationContext(Dispatcher.CurrentDispatcher));
+                    element = func();
+                    element.SourceInitialized += OnSourceInitialized;
+                    element.Show();
+                    are.Set();
+                    Dispatcher.Run();
+                }
+                catch (Exception ex)
+                {
+                    exception = ex;
+                }
+            });
+            thread.Name = "Back ground UI";
+            thread.IsBackground = true;
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+            await Task.Run(() =>
+            {
+                are.WaitOne();
+                are.Dispose();
+            });
+            if (exception != null)
+            {
+                ExceptionDispatchInfo.Capture(exception).Throw();
+            }
+            return element;
+        }
+
+        private static void OnSourceInitialized(object sender, EventArgs e)
+        {
+            var childHandle = new WindowInteropHelper((SubWindow)sender).Handle;
+            SetParent(childHandle, parentHwnd.Handle);
+            SetWindowLongA(childHandle, GWL_EXSTYLE, (int)WS_CHILD);// RIGHT
+            MoveWindow(childHandle, 0, 0, 300, 300, true);
+
+        }
+
+        private const int GWL_STYLE = (-16);
+        private const int WS_VISIBLE = 0x10000000;
+
+        /// <summary> 
+        /// window的扩展样式 
+        /// </summary> 
+        public const int GWL_EXSTYLE = -20;
+        public const long WS_CHILD = 0x40000000L;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern long SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongA", SetLastError = true)]
+        public static extern int SetWindowLongA([In()] System.IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool MoveWindow(IntPtr hwnd, int x, int y, int cx, int cy, bool repaint);
+    }
+}
+
---
+++ MultithreadedUI/NativeMethods.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadedUI
+{
+    public class NativeMethods
+    {
+        /// <summary> 
+        /// 带有外边框和标题的windows的样式 
+        /// </summary> 
+        public const long WS_CAPTION = 0x00C00000L;
+        public const long WS_CAPTION_2 = 0X00C0000L;
+        // public const long WS_BORDER = 0X0080000L; 
+
+        /// <summary> 
+        /// window 扩展样式 分层显示 
+        /// </summary> 
+        public const long WS_EX_LAYERED = 0x00080000L;
+        public const long WS_CHILD = 0x40000000L;
+
+        /// <summary> 
+        /// 带有alpha的样式 
+        /// </summary> 
+        public const long LWA_ALPHA = 0x00000002L;
+
+        /// <summary> 
+        /// 颜色设置 
+        /// </summary> 
+        public const long LWA_COLORKEY = 0x00000001L;
+
+        /// <summary> 
+        /// window的基本样式 
+        /// </summary> 
+        public const int GWL_STYLE = -16;
+
+        /// <summary> 
+        /// window的扩展样式 
+        /// </summary> 
+        public const int GWL_EXSTYLE = -20;
+
+        /// <summary> 
+        /// 设置窗体的样式 
+        /// </summary> 
+        /// <param name="handle">操作窗体的句柄</param> 
+        /// <param name="oldStyle">进行设置窗体的样式类型.</param> 
+        /// <param name="newStyle">新样式</param> 
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongA", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetWindowLongA(IntPtr handle, int oldStyle, long newStyle);
+
+        /// <summary> 
+        /// 获取窗体指定的样式. 
+        /// </summary> 
+        /// <param name="handle">操作窗体的句柄</param> 
+        /// <param name="style">要进行返回的样式</param> 
+        /// <returns>当前window的样式</returns> 
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern long GetWindowLong(IntPtr handle, int style);
+
+        /// <summary> 
+        /// 设置窗体的工作区域. 
+        /// </summary> 
+        /// <param name="handle">操作窗体的句柄.</param> 
+        /// <param name="handleRegion">操作窗体区域的句柄.</param> 
+        /// <param name="regraw">if set to <c>true</c> [regraw].</param> 
+        /// <returns>返回值</returns> 
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        public static extern int SetWindowRgn(IntPtr handle, IntPtr handleRegion, bool regraw);
+
+
+        //=================================================================================
+        /// <summary>
+        /// 设置窗体为无边框风格
+        /// </summary>
+        /// <param name="hWnd"></param>
+        public static void SetWindowNoBorder(IntPtr hWnd)
+        {
+            long oldstyle = GetWindowLong(hWnd, NativeMethods.GWL_STYLE);
+
+            SetWindowLongA(hWnd, GWL_STYLE, oldstyle & (~(WS_CAPTION | WS_CAPTION_2)));
+            //SetWindowLong(hWnd, GWL_EXSTYLE, WS_CHILD);
+        }
+    }
+
+}
